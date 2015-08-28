0921f670e4d21d54bff2851ff20147b7897b7900
Sarah Bird <sarah@bonvaya.com>
2015-08-28 14:59:13 +0100

    Add an example

---
+++ examples/embed/embed_multiple_responsive.py
@@ -0,0 +1,54 @@
+from bokeh.browserlib import view
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.resources import Resources
+from bokeh.templates import RESOURCES
+
+from jinja2 import Template
+import random
+
+########## BUILD FIGURES ################
+
+TOOLS = 'autoresize'  # We use the autoresize tool to make the plots responsive
+
+PLOT_OPTIONS = dict(tools=TOOLS, plot_width=800, plot_height=300)
+SCATTER_OPTIONS = dict(size=12, alpha=0.5)
+data = lambda: [random.choice([i for i in range(100)]) for r in range(10)]
+p1 = figure(**PLOT_OPTIONS).scatter(data(), data(), color="red", **SCATTER_OPTIONS)
+p2 = figure(**PLOT_OPTIONS).scatter(data(), data(), color="blue", **SCATTER_OPTIONS)
+p3 = figure(**PLOT_OPTIONS).scatter(data(), data(), color="green", **SCATTER_OPTIONS)
+
+########## RENDER PLOTS ################
+
+# Define our html template for out plots
+template = Template('''<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Responsive plots</title>
+        {{ plot_resources }}
+    </head>
+    <body>
+    <h2>Resize the window to see the plots resizing</h2>
+    {% for div in plot_div.values() %} {{ div }} {% endfor %}
+    {{ plot_script }}
+    </body>
+</html>
+''')
+
+# Setup out resources
+resources = Resources(mode='relative-dev')
+plot_resources = RESOURCES.render(
+    js_raw=resources.js_raw,
+    css_raw=resources.css_raw,
+    js_files=resources.js_files,
+    css_files=resources.css_files,
+)
+
+script, div = components({'Red': p1, 'Blue': p2, 'Green': p3})
+html = template.render(plot_resources=plot_resources, plot_script=script, plot_div=div)
+html_file = 'embed_multiple_responsive.html'
+with open(html_file, 'w') as f:
+    f.write(html)
+
+view(html_file)
